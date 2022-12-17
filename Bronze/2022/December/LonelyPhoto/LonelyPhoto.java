c81e7dc662a3618babbf9d4ac2df4fa868cf21ce
Piyush Acharya <acharyapiyush1@outlook.com>
2022-12-17 14:39:35 -0800

    Add some more methods

--- Bronze/2022/December/LonelyPhoto/LonelyPhoto.java
+++ Bronze/2022/December/LonelyPhoto/LonelyPhoto.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class LonelyPhoto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
+        String fakeNum = sc.nextLine();
         String characters = sc.nextLine();
-        sc.close();
+        long n = characters.length();
 
         long lonelyCount = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 2; j < n; j++) {
                 String substring = characters.substring(i, j + 1);
-                if ((substring.chars().filter(ch -> ch == 'G').count() == 1) || (substring.chars().filter(ch -> ch == 'C').count() == 1)) {
+                if (substring.chars().filter(ch -> ch == 'G').count() == 1
+                        || substring.chars().filter(ch -> ch == 'H').count() == 1) {
                     lonelyCount++;
                 }
             }
